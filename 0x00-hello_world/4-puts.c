c50a79c723b643b04226306481f2821b3667ba92
Juma Kim <j.i.kimingichi@gmail.com>
2023-02-16 06:05:05 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
-#Incluse <stdio.h>
+#include <stdio.h>
 /**
- *main - Entry point
- *Return: Always 0 (success)
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
