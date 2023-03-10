a5d289558504e447f8c432c2fd7d3a6b4315d71a
Juma Kim <j.i.kimingichi@gmail.com>
2023-03-10 12:10:05 -0800

    Taks

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,10 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-void _puts(char *str)
+void _puts(char *s)
 {
-	int main(void);
-
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts(s);
 }
