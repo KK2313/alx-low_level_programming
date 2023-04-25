ad9d3004ee5c98d14c440d8c4762ae84809cdf20
KK2313 <j.i.kimingichi@gmail.com>
2023-04-25 12:24:13 -0700

    Token Update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * _strlen - Returns the length of a string
- * @s: The string to get the length of
- * Return: The lenght of @s
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
-
 int _strlen(char *s)
 {
-	int i = 0;
+	int longi = 0;
 
-	while (s[i])
-	i++;
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
 
-	return (i);
+	return (longi);
+}
 }
